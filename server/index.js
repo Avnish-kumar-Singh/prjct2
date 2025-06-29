dcae29d110dd5710bb98c5cd7a419dfc5ef4a207
Avnish Kumar Singh <avnish.kumar.er@gmail.com>
2025-06-29 19:28:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import Contact from "./models/Contact.js"; // ✅ Your schema file
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors());
