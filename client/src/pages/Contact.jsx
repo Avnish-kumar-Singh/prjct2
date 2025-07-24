6441cbbd450687136d6d6dc22bf2ba542e78d1e3
Avnish Kumar Singh <avnish.kumar.er@gmail.com>
2025-07-24 21:59:04 +0530

    Update Contact.jsx

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -13,8 +13,8 @@ function Contact() {
     setLoading(true);
 
     // ✅ Correct fallback URL
-   // const apiUrl = import.meta.env.VITE_API_URL || "https://prjct2-1.onrender.com";
-   const apiUrl = import.meta.env.VITE_API_URL || "https://prjct2-uunw.onrender.com";
+    const apiUrl = import.meta.env.VITE_API_URL || "https://prjct2-1.onrender.com";
+   //const apiUrl = import.meta.env.VITE_API_URL || "https://prjct2-uunw.onrender.com";
 
 
     console.log("📤 Sending to:", `${apiUrl}/contact`);
